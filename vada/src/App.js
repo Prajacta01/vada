233effb1ec2867c1db20f49234de75473780fc53
Prajacta <prajacta@Prajactas-MacBook-Air.local>
2022-11-20 17:20:12 -0800

    text autogenerator; speech to text

---
+++ vada/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React, {useState} from 'react'
+import { useSpeechSynthesis } from 'react-speech-kit';
+
+function App() {
+  const [vada, setVada] = useState('eggg');
+  const changeText = () => {
+    setVada(generateRandomString())
+  }
+  const {speak} = useSpeechSynthesis();
+  const getRandomListElement = (strings) => {
+    return strings[Math.floor(Math.random() * strings.length)]
+  }
+
+  const generateRandomString = () => {
+    let stringsList = [
+      'hi',
+      'prajacta',
+      'egg',
+      'huevo',
+      'yay',
+      'potato',
+      'cringe',
+      'rip',
+      ''
+    ]
+
+    let final = ""
+    for (let i = 0; i <10; i++) {
+
+      final += getRandomListElement(stringsList) + ' '
+    }
+
+    return final;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          {vada}
+        </a>
+        <button onClick = {changeText}>change</button>
+        <button onClick = {() => speak({text:vada})}>speak</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
